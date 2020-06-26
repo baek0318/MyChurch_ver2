e94c6f6cf29af3c72d6b9bf119fe6d1c844ed1ad
peachberry <peachberry0318@gmail.com>
2020-06-27 01:27:57 +0900

    iOS14 MKMapView를 LocationViewController에 추가

--- MyChurch_ver2/SubViewController/SubMoreViewController/LocationViewController.swift
+++ MyChurch_ver2/SubViewController/SubMoreViewController/LocationViewController.swift
@@ -8,18 +8,76 @@
 
 import UIKit
 import MapKit
+import Contacts
 
 class LocationViewController : UIViewController {
     
     @IBOutlet var titleName: UILabel!
     var saveTitle : String?
     
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         self.titleName.text = saveTitle
+        addMap()
         super.viewDidLoad()
     }
     
     @IBAction func moveToBack(_ sender: Any) {
         self.dismiss(animated: true)
+        
+    }
+    
+    func addMap() {
+        mapView.delegate = self
+        let initialLocation = CLLocation(latitude: 35.8235775, longitude: 128.521810)
+        mapView.centerToLocation(initialLocation)
+        
+        mapView.addAnnotation(Annotation(title: "대흥교회", locationName: "대구를 사랑하는 대흥교회" ,coordinate: CLLocationCoordinate2D(latitude: 35.8235775, longitude: 128.521810)))
+    }
+}
+
+//MARK:- extension MKMapView
+
+private extension MKMapView {
+    func centerToLocation(_ location : CLLocation, regionRadius : CLLocationDistance = 200){
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension LocationViewController : MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let annotation = view.annotation as? Annotation else {return}
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+        annotation.mapItem?.openInMaps(launchOptions: launchOptions)
+    }
+}
+//MARK:- MKAnnotation
+
+class Annotation : NSObject, MKAnnotation {
+    let title : String?
+    let locationName : String?
+    let coordinate : CLLocationCoordinate2D
+    
+    init(title : String?, locationName : String? ,coordinate : CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.coordinate = coordinate
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+    
+    var mapItem : MKMapItem? {
+        guard let location = locationName else {return nil}
+        
+        let addressDict = [CNPostalAddressStreetKey : location]
+        let placeMark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDict)
+        let mapItem = MKMapItem(placemark: placeMark)
+        mapItem.name = title
+        return mapItem
     }
 }
