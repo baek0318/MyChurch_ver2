c81d498eb0882396dc080f857e616eadf86ed6b6
peachberry <peachberry0318@gmail.com>
2020-07-12 00:55:56 +0900

    logoButton 생성과 iPhone se1에 대응하는 ui수정

---
+++ MyChurch_ver2/SubViewController/OldDataViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OldDataViewController.swift
+//  MyChurch_ver2
+//
+//  Created by 백승화 on 2020/07/11.
+//  Copyright © 2020 백승화. All rights reserved.
+//
+
+import UIKit
+
+class OldDataViewController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
---
+++ MyChurch_ver2/SubViewController/OldSermonViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OldSermonViewController.swift
+//  MyChurch_ver2
+//
+//  Created by 백승화 on 2020/07/12.
+//  Copyright © 2020 백승화. All rights reserved.
+//
+
+import UIKit
+
+class OldSermonViewController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
